3c3f1fe91abd36d050ac2b9666a2f7dd1683ba4b
Jefferson Charlles <contato@jefferdeveloper.com>
2022-07-06 16:02:25 -0300

    chore:create table

--- web/src/components/TransactionsTable/index.tsx
+++ web/src/components/TransactionsTable/index.tsx
@@ -6,6 +6,30 @@ export const TransactionsTable = () => {
 
     return (
         <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de webSite</td>
+                        <td className='deposit'>R$12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>06/07/2022</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className='withdraw'>-R$1.200</td>
+                        <td>Casa</td>
+                        <td>02/07/2022</td>
+                    </tr>
+                </tbody>
+            </table>
 
         </Container>
     );
